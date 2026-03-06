b70edd7aa709a1486e34ccec549e2c0887f80ee5
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-06 11:30:18 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const nummeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero= .5;
+
+const novaOperacao = primeiroNumero / nummeroPontoFlutuante;
+
+console. log(novaOperacao);
+
+// naN -> Not a number (nao e numero)
+
+const alura = "Alura";
+
+console. log(alura * primeiroNumero);
