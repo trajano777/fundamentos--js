7a93725980de092c61dc5354a76837262877d0cd
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-06 14:56:09 +0000

    tipos primitivos

---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Ola, mundo!";
+const texto2 = 'Ola mundo!';
+
+const senha = "senhaSuperSegura456";
+
+const stringDeNumeros = "3456";
+
+const citacao = "Meu nome é ";
+const meuNome = "trajano";
+
+console. log(citacao + meuNome);
