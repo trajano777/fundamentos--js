ad2c18566a6a780db80a8b6177b09c56229610dd
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-10 14:15:33 +0000

    variaveis

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+// boolean
+//true -> verdadeiro
+//false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const testo1 = "alura";
+const texto2 = "alura"
+
+console.log(textoq === texto2);
---
+++ var-let-const.js
@@ -0,0 +1,21 @@
+// var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura *comprimento;
+//console.log(area);
+//var area;
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+ let area;
+
+ //se (forma igual 'retangulo')faça
+ if (forma === 'forma retangulo'){
+    area = altura comprimento;
+}else {
+    area = (altura * comprimento)/ 2;
+}
+console.log(area);
