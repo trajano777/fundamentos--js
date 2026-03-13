7bc5b9a9ed9f201eaf5a34b3990c2fed25265908
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-13 15:03:43 +0000

    template

--- template-strings.js
+++ template-strings.js
@@ -1,3 +1,8 @@
 const nome = "ju";
 const idade = 2026-1981;
 onst cidadenascimento = "pitanga";
+
+const apresentacao = "meu nome e "+ nome + ", minha idade e" + idade +" e nasci na cidade de" +cidadedenascimento;
+
+const apresentacao = `meu nome e ${nome}, minha idade e ${idade}e nasci na cidade de ${cidadedenascimento }`
+console.log(apresentacao);
