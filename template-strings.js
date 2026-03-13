f963f5daba3b73e482ca639ee176502ceb0101fb
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-13 10:46:26 +0000

    template

---
+++ template-strings.js
@@ -0,0 +1,3 @@
+const nome = "ju";
+const idade = 2026-1981;
+onst cidadenascimento = "pitanga";
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const inademinima = 18;
+const idadecliente = 16;
+
+//se idadecliente for maior que idademinima entao faça
+if(idadecliente >= idademinima){
+    console.log("cerveja");
+    }else{
+        console.log("suco");
+    }
+                            //condicao          "true"      "false"
+    console.log(idadecliente >= idademinima ? "cervea" : "suco")
